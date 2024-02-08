5e5d88110735502000515b1075bfa62939ad2d84
John Dungao <johnmiguel.dungao@gmail.com>
2024-02-08 16:39:01 -0500

    finish react-router, add navigation and link

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Blue from "./components/Blue";
+import Red from "./components/Red";
+import Home from "./components/Home";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/blue">Blue</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
